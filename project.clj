a30fc27c8344c7b71cc4899a5551d84129673d42
Sergey Savenko <deadok22@gmail.com>
2015-05-22 14:41:35 +0300

    v0.1.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-jsr223 "0.1.1"
+(defproject clj-jsr223 "0.1.2"
             :description "A JSR 223 implementation written in Clojure."
             :url "https://github.com/eduardoejp/clj-jsr223"
             :license {:name "Eclipse Public License - v 1.0"
