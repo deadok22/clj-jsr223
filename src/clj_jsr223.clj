56ae386bf1a51db4ce50dfaa56b11621805c0764
Sergey Savenko <deadok22@gmail.com>
2015-05-22 14:01:48 +0300

    cleanup

--- src/clj_jsr223.clj
+++ src/clj_jsr223.clj
@@ -16,19 +16,20 @@
   ^{:author "Eduardo Julián <eduardoejp@gmail.com>",
     :doc "Clojure implementations of the ScriptEngine and ScriptEngineFactory interfaces."}
   clj-jsr223
-  (:import (javax.script Bindings ScriptContext)))
+  (:import (javax.script Bindings ScriptContext ScriptEngine SimpleBindings ScriptEngineFactory SimpleScriptContext)
+           (java.io Reader)))
 
 (deftype ClojureScriptEngine [factory context]
-  javax.script.ScriptEngine
+  ScriptEngine
   (getFactory [_] factory)
   (getContext [_] @context)
   (setContext [_ ctx] (reset! context ctx))
-  (createBindings [_] (javax.script.SimpleBindings.))
+  (createBindings [_] (SimpleBindings.))
   (getBindings [_ ctx] (.getBindings @context ctx))
   (setBindings [_ binds ctx] (.setBindings @context binds ctx))
   (put [self k v] (.put (.getBindings self) k v))
   (eval ^Object [self ^String script] (.eval self script @context))
-  (eval ^Object [self ^String script ^Bindings binds]
+  (eval ^Object [_ ^String script ^Bindings binds]
     (doseq [[k v] (->> binds seq (apply conj {}))]
       (let [parts (.split k "/")
             [ns name] (if (= 2 (count parts)) (map symbol parts) ['user (symbol (first parts))])]
@@ -39,14 +40,14 @@
       (eval (read-string (clojure.string/join (list "(do " script ")"))))
       )
     )
-  (eval ^Object [self ^String script ^ScriptContext ctx] (.eval self script (.getBindings ctx javax.script.ScriptContext/ENGINE_SCOPE)))
-  (eval ^Object [self ^java.io.Reader script] (.eval self (slurp script)))
-  (eval ^Object [self ^java.io.Reader script ^Bindings binds] (.eval self (slurp script) binds))
-  (eval ^Object [self ^java.io.Reader script ^ScriptContext ctx] (.eval self (slurp script) (.getBindings ctx javax.script.ScriptContext/ENGINE_SCOPE)))
+  (eval ^Object [self ^String script ^ScriptContext ctx] (.eval self script (.getBindings ctx ScriptContext/ENGINE_SCOPE)))
+  (eval ^Object [self ^Reader script] (.eval self (slurp script)))
+  (eval ^Object [self ^Reader script ^Bindings binds] (.eval self (slurp script) binds))
+  (eval ^Object [self ^Reader script ^ScriptContext ctx] (.eval self (slurp script) (.getBindings ctx ScriptContext/ENGINE_SCOPE)))
   )
 
 (deftype ClojureScriptEngineFactory []
-  javax.script.ScriptEngineFactory
+  ScriptEngineFactory
   (getParameter [_ _] nil)
   (getEngineName [_] "Clojure Scripting Engine")
   (getEngineVersion [_] "1.4")
@@ -55,7 +56,7 @@
   (getExtensions [_] ["clj"])
   (getMimeTypes [_] ["application/clojure" "text/clojure"])
   (getNames [_] ["clojure" "Clojure"])
-  (getScriptEngine [self] (ClojureScriptEngine. self (atom (javax.script.SimpleScriptContext.))))
+  (getScriptEngine [self] (ClojureScriptEngine. self (atom (SimpleScriptContext.))))
   (getOutputStatement [_ to-print] (str "(println \"" to-print "\")"))
   (getMethodCallSyntax [_ obj meth args] (str "(." meth " " obj " " (apply str (interpose " " (map pr-str args))) ")"))
   (getProgram [_ forms] (apply str (interleave forms (repeat \newline)))))
